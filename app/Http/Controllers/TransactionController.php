e5aa92ec13a9bc5ff274cc5a42c488d500176c53
rahmathub <rahmatwijayanto10@gmail.com>
2023-07-12 18:49:11 +0700

    edit data transactions done

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -141,16 +141,98 @@ class TransactionController extends Controller
      */
     public function edit(Transaction $transaction)
     {
-        return view('admin.transaction.edit');
+        $costumerName = Customer::all();
+        $products = Product::all();
+        $transactionDetail = TransactionDetail::all();
+        $selectedProductIds = $transaction->transactionDetails->pluck('product_id')->toArray();
+    
+        return view('admin.transaction.edit', compact('costumerName', 'products', 'transaction', 'transactionDetail', 'selectedProductIds'));
     }
+    
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Transaction $transaction)
     {
-        //
+        // Validasi input
+        $validator = Validator::make($request->all(), [
+            'customer_id' => ['required', 'numeric'],
+            'transaction_datetime' => ['required', 'date'],
+            'product_id' => ['required', 'array'],
+            'product_id.*' => ['required', 'numeric'],
+            'quantity' => ['required', 'array'],
+            'quantity.*' => ['required', 'numeric'],
+            'price_total' => ['required', 'numeric'],
+            'accept_customer_money' => ['required', 'numeric'],
+            'change_customer_money' => ['required', 'numeric'],
+        ]);
+    
+        // Jika validasi gagal, kembalikan respon dengan pesan error
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+    
+        // Mendapatkan input dari request
+        $customer_id = $request->input('customer_id');
+        $transaction_datetime = $request->input('transaction_datetime');
+        $product_ids = $request->input('product_id');
+        $quantities = $request->input('quantity');
+        $price_total = $request->input('price_total');
+        $accept_customer_money = $request->input('accept_customer_money');
+        $change_customer_money = $request->input('change_customer_money');
+    
+        // Mengupdate data transaksi
+        $transaction->customer_id = $customer_id;
+        $transaction->transaction_datetime = date('Y-m-d H:i:s', strtotime($transaction_datetime));
+        $transaction->price_total = $price_total;
+        $transaction->accept_customer_money = $accept_customer_money;
+        $transaction->change_customer_money = $change_customer_money;
+    
+        // Menghitung total produk
+        $product_total = count($product_ids);
+        $transaction->product_total = $product_total;
+    
+        // Menghapus detail transaksi yang ada
+        $transaction->transactionDetails()->delete();
+    
+        // Menyimpan detail transaksi yang baru
+        foreach ($product_ids as $index => $product_id) {
+            if (isset($quantities[$index])) {
+                $quantity = $quantities[$index];
+    
+                // Membuat detail transaksi baru
+                $transactionDetail = new TransactionDetail();
+                $transactionDetail->transaction_id = $transaction->id;
+                $transactionDetail->product_id = $product_id;
+                $transactionDetail->qty = $quantity;
+    
+                // Mendapatkan harga produk dari database
+                $product = Product::find($product_id);
+                $transactionDetail->price = $product->price_deal;
+    
+                try {
+                    // Simpan data transaksi detail ke database
+                    $transactionDetail->save();
+    
+                    // Mengurangi stok produk berdasarkan jumlah yang dibeli
+                    $product->stock -= $quantity;
+                    $product->save();
+                } catch (QueryException $e) {
+                    // Menangkap error saat update data
+                    // Cetak pesan error untuk membantu mendiagnosis masalah
+                    dd($e->getMessage());
+                }
+            }
+        }
+    
+        // Simpan perubahan data transaksi
+        $transaction->save();
+    
+        // Redirect atau lakukan tindakan selanjutnya
+        return redirect()->route('transactions.index');
     }
+    
 
     /**
      * Remove the specified resource from storage.
