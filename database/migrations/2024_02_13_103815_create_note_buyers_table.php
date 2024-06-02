cf899bdbd80c0bceebe1b410be21ef3be0c00401
rahmathub <rahmatwijayanto10@gmail.com>
2024-06-02 21:35:33 +0700

    restore file delete because mistake

---
+++ database/migrations/2024_02_13_103815_create_note_buyers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('note_buyers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('store_id');
+            $table->string('photo');
+            $table->datetime('tanggal_pembelian'); // Menggabungkan tanggal dan waktu menjadi satu kolom
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('store_id')->references('id')->on('stores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('note_buyers');
+    }
+};
